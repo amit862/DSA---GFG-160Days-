ae33617a5cfa26cc55c609400296a4cb01b76a04
Amit Verma <77237968+amit862@users.noreply.github.com>
2026-04-13 22:20:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Cost Climbing Stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,16 @@
+//Back-end complete function Template for Java
+
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        // Write your code here
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+        
+        for(int i = 2; i < cost.length; i++) {
+            int curr = cost[i] + Math.min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return Math.min(prev1, prev2);
+    }
+};
