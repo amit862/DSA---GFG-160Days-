11329af85343b60a6029782048d6b3e44697fd5d
Amit Verma <77237968+amit862@users.noreply.github.com>
2026-04-27 22:29:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a row-wise sorted matrix/search-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,37 @@
+
+
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchRowMatrix(int[][] mat, int x) {
+        // code here
+        int n = mat.length;
+        int m = mat[0].length;
+        
+        for(int i = 0; i < n; i++) {
+            
+            if(x >= mat[i][0] && x <= mat[i][m - 1]) {
+                if(binarySearch(mat[i], x)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean binarySearch(int[] row, int x) {
+        int left = 0, right = row.length - 1;
+        
+        while(left<= right) {
+            int mid = left + (right - left) / 2;
+            
+            if(row[mid] == x) {
+                return true;
+            } else if(row[mid] < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
