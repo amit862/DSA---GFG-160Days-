20e028046dbaf56c0e5e01ba4b5881b6cc4a4d7d
Amit Verma <77237968+amit862@users.noreply.github.com>
2026-05-01 23:22:13 +0530

    Update equilibrium-point.java

--- Difficulty: Easy/Equilibrium Point/equilibrium-point.java
+++ Difficulty: Easy/Equilibrium Point/equilibrium-point.java
@@ -6,8 +6,10 @@ class Solution {
     for(int num : arr) {
         totalSum += num;
     }
+
     
     int leftSum = 0;
+
     
     for(int i = 0; i < arr.length; i++) {
         int rightSum = totalSum - leftSum - arr[i];
