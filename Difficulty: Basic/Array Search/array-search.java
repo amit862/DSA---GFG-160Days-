f9c27339933e89240f5bb6a684047af42c1c9ac1
Amit Verma <77237968+amit862@users.noreply.github.com>
2026-05-04 23:00:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Search/array-search.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int search(int[] arr, int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i; // first occurrence
+            }
+        }
+        return -1; // not found
+    }
+}
