ba6a135438ddec85496a5d871fe92cde065d5ce5
Amit Verma <77237968+amit862@users.noreply.github.com>
2026-05-24 21:40:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Occurrence in Sorted/first-occurrence-in-sorted.java
@@ -0,0 +1,23 @@
+class Solution {
+    int firstSearch(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) {
+                ans = mid;
+                high = mid - 1; // search left side
+            } 
+            else if (arr[mid] < k) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
